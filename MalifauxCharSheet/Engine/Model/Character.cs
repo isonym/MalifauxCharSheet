b81f47ab0953b21123b6aa8c7c869a4f9f0ca532
James Graham <J.A.Graham@live.co.uk>
2016-12-29 17:12:38 +0000

    Add initial classes to engine project

---
+++ MalifauxCharSheet/Engine/Model/Character.cs
@@ -0,0 +1,27 @@
+﻿namespace Engine.Model
+{
+    public class Character
+    {
+        private string _name;
+        private string _playerName;
+        private double _scrip;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string PlayerName
+        {
+            get { return _playerName; }
+            set { _playerName = value; }
+        }
+
+        public double Scrip
+        {
+            get { return _scrip; }
+            set { _scrip = value; }
+        }
+    }
+}
---
+++ MalifauxCharSheet/Engine/Model/Magia.cs
@@ -0,0 +1,7 @@
+﻿namespace Engine.Model
+{
+    public class Magia
+    {
+        
+    }
+}
